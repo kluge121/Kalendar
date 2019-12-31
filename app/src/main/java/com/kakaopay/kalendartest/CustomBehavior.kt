dfee71d665e2bff4760a280dcc8b0c6ca1f6204a
benny <benny.egg@kakaopaycorp.com>
2019-12-31 17:02:58 +0900

    클래스 생성

--- app/src/main/java/com/kakaopay/kalendartest/CustomBehavior.kt
+++ app/src/main/java/com/kakaopay/kalendartest/CustomBehavior.kt
@@ -1,2 +1,13 @@
 package com.kakaopay.kalendartest
 
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.doOnLayout
+
+
+class CalendarCollapsBehavior : CoordinatorLayout.Behavior<View>() {
+
+
+    
+
+}
